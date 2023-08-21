db5ecea5d182c489f0da3c47a43fed9b6137b79a
om2013ab <omarbashaiwth@gmail.com>
2023-08-21 21:00:23 +0800

    Fixed the bugs in the y-axis titles' format in the bar_chart.dart

--- lib/reports/view/widgets/bar_chart.dart
+++ lib/reports/view/widgets/bar_chart.dart
@@ -5,50 +5,65 @@ import '../../model/monthly_report_model.dart';
 
 class DrawBarChart extends StatelessWidget {
   final List<MonthlyReportModel> data;
+
   const DrawBarChart({Key? key, required this.data}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return BarChart(BarChartData(
-      titlesData: FlTitlesData(
-          topTitles: AxisTitles(axisNameWidget: null),
-          rightTitles: AxisTitles(axisNameWidget: null),
-          bottomTitles: AxisTitles(
-            sideTitles: SideTitles(
+        barTouchData: BarTouchData(
+            enabled: true,
+            touchTooltipData: BarTouchTooltipData(
+                getTooltipItem: (group, groupIndex, rod, rodIndex) {
+              return BarTooltipItem(
+                  rod.toY.round().toString(),
+                  const TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+              );
+            }),
+        ),
+        titlesData: FlTitlesData(
+            topTitles: AxisTitles(axisNameWidget: null),
+            leftTitles: AxisTitles(
+                sideTitles: SideTitles(
+                  reservedSize: 30,
               showTitles: true,
-              getTitlesWidget:(value, _) => Padding(
-                  padding: const EdgeInsets.only(top: 8),
-                  child: Text(data[value.toInt()].month, style: const TextStyle(fontFamily: 'Tajawal', fontSize: 11),),
+              getTitlesWidget: (value,meta) {
+                    return Text(value == meta.max? '' : meta.formattedValue);
+              }
+            )),
+            rightTitles: AxisTitles(axisNameWidget: null),
+            bottomTitles: AxisTitles(
+                sideTitles: SideTitles(
+              showTitles: true,
+              getTitlesWidget: (value, _) => Padding(
+                padding: const EdgeInsets.only(top: 8),
+                child: Text(
+                  data[value.toInt()].month,
+                  style: const TextStyle(fontFamily: 'Tajawal', fontSize: 11),
                 ),
-            )
-          )
-      ),
-      borderData: FlBorderData(show: false),
+              ),
+            ))),
+        borderData: FlBorderData(show: false),
         gridData: FlGridData(drawVerticalLine: false),
-        barGroups: _buildBars(data: data)
-    )
-    );
-
+        barGroups: _buildBars(data: data)));
   }
 
-  List<BarChartGroupData> _buildBars({required List<MonthlyReportModel> data,double width = 12}){
-    return List.generate(5, (index) =>
-        BarChartGroupData(
-          x: index,
-          barRods: [
-            BarChartRodData(
-              toY: data[index].incomes,
-              color: const Color(0xFFF7ADA7),
-              width: width
-            ),
-            BarChartRodData(
-                toY: data[index].expenses,
-                color: const Color(0xFFEF4A37),
-                width: width
-            ),
-          ]
-        )
-    );
+  List<BarChartGroupData> _buildBars(
+      {required List<MonthlyReportModel> data, double width = 12}) {
+    return List.generate(
+        data.length,
+        (index) => BarChartGroupData(
+            x: index,
+            barRods: [
+              BarChartRodData(
+                  toY: data[index].incomes,
+                  color: const Color(0xFFF7ADA7),
+                  width: width),
+              BarChartRodData(
+                  toY: data[index].expenses,
+                  color: const Color(0xFFEF4A37),
+                  width: width),
+            ]));
   }
-
 }
